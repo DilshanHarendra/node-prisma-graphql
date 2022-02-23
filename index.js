0883b45539d17f7b8c793d315314441d65d0e495
DilshanHarendra <harendracripto@gmail.com>
2022-02-23 23:57:44 +0530

    add graphql

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
 const express = require('express')
+const {PrismaClient} = require('@prisma/client')
+const {graphqlHTTP} = require('express-graphql')
+const {makeExecutableSchema}= require('@graphql-tools/schema')
+
 const app = express()
+const prisma = new PrismaClient()
+
+const typeDefs=`
+    
+    type Books{
+        title: String!
+        category : String 
+    }
+    
+    type Query{
+        allBooks:[Books!]!
+    }
+`
+const resolvers={
+    Query:{
+        allBooks:()=>{
+            return prisma.books.findMany()
+        }
+    }
+}
+
+ const schema = makeExecutableSchema({
+    resolvers,
+    typeDefs
+})
+
+app.use('/graphql',graphqlHTTP({
+    schema
+}))
 
 
 app.get('/',(req,res)=>{
